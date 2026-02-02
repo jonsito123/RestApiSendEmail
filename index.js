eeb25e8f56360d7e1c7d4983d5371f462d132944
jonsito123 <ecuevah70@gmail.com>
2026-02-02 11:19:53 -0500

    version1

---
+++ index.js
@@ -0,0 +1,65 @@
+const express=require("express")
+const app=express();
+var axios = require('axios');
+var qs = require('qs');
+
+require("dotenv").config();
+
+app.use(express.json())
+
+app.get("/",(req,res)=>{
+
+  res.send("EnviarEmailPrueba")
+})
+app.post("/EnviarWasap",(req,res)=>{
+
+
+const body=req.body;
+
+
+var data = qs.stringify({
+    "token": process.env.KEY,
+    "to": `+51${body.Celular}`,
+    "body": `${body.Mensaje}`
+});
+
+var config = {
+  method: 'post',
+  url: 'https://api.ultramsg.com/instance160710/messages/chat',
+  headers: {  
+    'Content-Type': 'application/x-www-form-urlencoded'
+  },
+  data : data
+};
+
+axios(config)
+.then(function (response) {
+  console.log(JSON.stringify(response.data));
+
+  res.send({
+
+   Estado:1,
+   mensaje:"Enviado correctamente el mensaje"
+
+  })
+})
+.catch(function (error) {
+  console.log(error);
+  res.send({
+    Estado:0,
+    mensaje:"Error al enviar el mensaje"
+    
+
+
+  })
+});
+
+
+
+})
+
+
+app.listen(5000,()=>{
+
+    console.log("Recorreindo en el puerto 5000")
+})
